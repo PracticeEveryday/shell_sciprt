0345b5c7396334164f64548a212b76f37903d278
PracticeEveryday <97580759+PracticeEveryday@users.noreply.github.com>
2022-11-18 13:25:36 +0900

    Create dbSetting.sh

---
+++ dbSetting.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo yum update -y
+echo "yum update succ"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+echo "nvm install succ"
+
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
+echo "Node version check"
+
+npm install yarn
+echo "yarn install succ"
+
+sudo yum install git -y
+echo "git install succ"
+
+sudo yum install mariadb-server
+echo "mariadb install succ"
+
+mysql —version
+echo "mariadb version"
+
+sudo systemctl enable mariadb
+echo "mariadb infinite"
+
+sudo systemctl start mariadb
+ehco "mariadb start"
